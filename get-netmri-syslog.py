0c263d20b592358c2e16c8eb1e79f0cf8d9d5db5
Chris Hindy <chindy@empowerednetworks.com>
2021-02-05 12:32:12 -0500

    Fixed for python 3

--- get-netmri-syslog.py
+++ get-netmri-syslog.py
@@ -9,7 +9,7 @@ def post_request(user_name, password, base_url, api_path, payload):
     """Send the request to NetMRI and catch the response coming back."""
     from requests.auth import HTTPBasicAuth
     url = base_url + api_path
-    response = requests.post(url, auth=HTTPBasicAuth(user_name, password), \
+    response = requests.post(url, auth=HTTPBasicAuth(user_name, password), 
         json=payload)
     return response
 
@@ -17,31 +17,33 @@ def get_request(user_name, password, base_url, api_path, payload):
     """Send the request to NetMRI and catch the response coming back."""
     from requests.auth import HTTPBasicAuth
     url = base_url + api_path
+    check_cert = False
     if payload == "":
-        response = requests.get(url, auth=HTTPBasicAuth(user_name, password))
+        response = requests.get(url, auth=HTTPBasicAuth(user_name, password),
+	verify=check_cert)
     else:
-        response = requests.get(url, auth=HTTPBasicAuth(user_name, password), \
-        json=payload)
+        response = requests.get(url, auth=HTTPBasicAuth(user_name, password), 
+        json=payload, verify=check_cert)
     return response
 
 def collect_config_info():
     """ ask the user for info about NetMRI, users, passwords, etc. """
     default_host = "netmri.lwpca.net"
-    netmri_host = raw_input("Please enter NetMRI hostname: [%s] " %(default_host))
+    netmri_host = input("Please enter NetMRI hostname: [%s] " %(default_host))
     if not netmri_host:
         netmri_host = default_host
-    netmri_admin_pwd = getpass.getpass('NetMRI admin user password: ')
+    netmri_admin_pwd = getpass.getpass('NetMRI admin user password: ', stream=None)
     netmri_base_url = "https://" + netmri_host + "/api"
-    return({"admin_name" : "admin", "admin_pass" : netmri_admin_pwd, "netmri_host": netmri_host, \
+    return({"admin_name" : "admin", "admin_pass" : netmri_admin_pwd, "netmri_host": netmri_host, 
     	"base_url": netmri_base_url})
 
 def admin_account_works(collected_input):
     """ Check we can connect to NetMRI using the supplied credentials. """
-    result = get_request(collected_input['admin_name'], collected_input['admin_pass'], \
+    result = get_request(collected_input['admin_name'], collected_input['admin_pass'], 
         collected_input['base_url'], '/', "")
     if result.status_code == 403:
-        print "Unable to authenticate to NetMRI using user 'admin' and the supplied password."
-        print "Please check the password and try again.  Exiting."
+        print("Unable to authenticate to NetMRI using user 'admin' and the supplied password.")
+        print("Please check the password and try again.  Exiting.")
         sys.exit(1)
 
 
@@ -51,9 +53,9 @@ def main():
     # Check if we can authenticate with the admin account.
     admin_account_works(collected_input)
     base_url = "https://%s/" %(collected_input["netmri_host"])
-    syslog_doc = get_request(collected_input["admin_name"], collected_input["admin_pass"], \
+    syslog_doc = get_request(collected_input["admin_name"], collected_input["admin_pass"], 
     	base_url, "netmri/api/change/syslog-config.tdf?contentType=text/json", "")
-    print syslog_doc.text
+    print(syslog_doc.text)
 
 if __name__ == "__main__":
     main()
